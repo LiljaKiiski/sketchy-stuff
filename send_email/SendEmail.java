1aea12aa63ad305cdc9442faa2719bcbc1cf9db6
LiljaK <lilja.maria.kiiski@gmail.com>
2022-04-20 20:24:04 -0700

    No connection error, but gets stuck when Transport tries to send message

--- send_email/SendEmail.java
+++ send_email/SendEmail.java
@@ -15,7 +15,7 @@ public class SendEmail {
       String from = "lilja.maria.kiiski@gmail.com";
 
       // Assuming you are sending email from localhost
-      String host = "10.0.0.155";
+      String host = "smtp.gmail.com";
 
       // Get system properties
       Properties properties = System.getProperties();
@@ -27,19 +27,14 @@ public class SendEmail {
       Session session = Session.getDefaultInstance(properties);
 
       try {
-         // Create a default MimeMessage object.
          MimeMessage message = new MimeMessage(session);
 
-         // Set From: header field of the header.
          message.setFrom(new InternetAddress(from));
 
-         // Set To: header field of the header.
          message.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
 
-         // Set Subject: header field
          message.setSubject("Java Test");
 
-         // Now set the actual message
          message.setText("Hey! This is Lilja! This message was sent with java :)");
 
          // Send message
