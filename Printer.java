0cdbff352ea8f5b92311ad04b25d4b8639190e1f
Lilja Kiiski <lilja.maria.kiiski@gmail.com>
2022-06-17 13:13:19 -0700

    Modified printer program

--- Printer.java
+++ Printer.java
@@ -1,6 +1,8 @@
 import java.io.FileInputStream;
 import java.io.IOException;
 
+import java.util.Scanner;
+
 import javax.print.*;
 import javax.print.attribute.*;
 import javax.print.attribute.standard.Copies;
@@ -9,30 +11,46 @@ import javax.print.event.*;
 public class Printer {
 
    	public static void main(String[] args) throws PrintException, IOException{
+		Scanner in = new Scanner(System.in);
 
-      		PrintService ps = PrintServiceLookup.lookupDefaultPrintService();
-		ps = java.awt.print.PrinterJob.lookupPrintServices()[0];
+      		PrintService ps = java.awt.print.PrinterJob.lookupPrintServices()[0];
+		System.out.println("Printer: " + ps.getName());
 
-      		System.out.println(ps.getName());
-
-		DocPrintJob job=ps.createPrintJob();
+		DocPrintJob job = ps.createPrintJob();
       		
 		job.addPrintJobListener(new PrintJobAdapter() {
       		
 			public void printDataTransferCompleted(PrintJobEvent event){
-        			System.out.println("data transfer complete");
+        			
       			}
 
       			public void printJobNoMoreEvents(PrintJobEvent event){
-        		    	System.out.println("received no more events");
+        		    	
          		}
       		});
       
-		String fileName = "filename.png";
-		FileInputStream fis=new FileInputStream(fileName);
-      		Doc doc=new SimpleDoc(fis, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
-      		PrintRequestAttributeSet attrib=new HashPrintRequestAttributeSet();
-      		attrib.add(new Copies(1));
-      		job.print(doc, attrib);
+		System.out.print("File to print: ");
+		String fileName = in.nextLine();
+		FileInputStream fis = null;
+
+		try {
+			fis = new FileInputStream(fileName);
+		
+		} catch (java.io.FileNotFoundException e){
+			System.out.println("Sorry, couldn't find that file. Please run the program again");
+			System.exit(0);
+		}
+
+		Doc doc=new SimpleDoc(fis, DocFlavor.INPUT_STREAM.AUTOSENSE, null);
+      		
+		PrintRequestAttributeSet attrib=new HashPrintRequestAttributeSet();
+      		
+		System.out.print("Copies: ");
+		int copies = in.nextInt();
+
+		attrib.add(new Copies(copies));
+      		
+		job.print(doc, attrib);
+		System.out.println("Printing your document");
    	}
 }
